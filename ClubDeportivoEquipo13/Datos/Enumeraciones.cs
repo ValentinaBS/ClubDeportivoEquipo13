ba4fa4df044d3cddad6c64d1cc2ea5cc33fcdb88
celina-web <celinatarn96@gmail.com>
2025-10-25 16:25:18 -0300

    Arreglo de bugs menores Enums aplicados

---
+++ ClubDeportivoEquipo13/Datos/Enumeraciones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDeportivoEquipo13.Datos
+{
+    enum TiposDePago
+    {
+        [Description("Efectivo")]
+        Efectivo,
+
+        [Description("Tarjeta de Crédito")]
+        TarjetaDeCredito,
+
+        [Description("Tarjeta de Débito")]
+        TarjetaDeDebito
+    }
+
+    enum TiposDeRegistro
+    {
+        [Description("Socio")]
+        Socio,
+
+        [Description("No-Socio")]
+        NoSocio
+    }
+}
