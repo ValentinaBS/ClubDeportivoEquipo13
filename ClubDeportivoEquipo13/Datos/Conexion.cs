36179128bd341c043153b11a404ae9b5353541a1
Agu <aguscp91@gmail.com>
2025-11-12 19:18:36 -0300

    Implementación cuadros de diálogo para sobreescribir los datos de la conexión de la base

--- ClubDeportivoEquipo13/Datos/Conexion.cs
+++ ClubDeportivoEquipo13/Datos/Conexion.cs
@@ -1,13 +1,62 @@
-﻿using System;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
 
 namespace ClubDeportivoEquipo13.Datos
 {
     public class Conexion
     {
+        private string Database;
+        private string server;
+        private string port;
+        private string user;
+        private string clave;
         private static Conexion instancia = null;
+        
+        private Conexion() {
 
-        private Conexion() { }
+            bool correcto = false;
+            int mensaje;
+
+            string T_server = "Server";
+            string T_user = "User";
+            string T_password = "Password";
+            string T_port = "Port";
+
+            while (correcto != true)
+            {
+                // Armamos los cuadros de dialogo para el ingreso de datos
+                T_server = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese servidor", "DATOS DE INSTALACIÓN MySQL");
+                T_user = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese usuario", "DATOS DE INSTALACIÓN MySQL");
+                T_password = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese clave", "DATOS DE INSTALACIÓN MySQL");
+                T_port = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese puerto", "DATOS DE INSTALACIÓN MySQL");
+
+                mensaje = (int)MessageBox.Show("su ingreso: SERVIDOR = " + T_server +
+                    " USUARIO: " + T_user +
+                    " CLAVE: " + T_password +
+                    " PUERTO= " + T_port, 
+                    "AVISO DEL SISTEMA", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (mensaje != 6) // el valor 6 corresponde al SI
+                {
+                    MessageBox.Show("INGRESE NUEVAMENTE LOS DATOS");
+                    correcto = false;
+                }
+                else
+                {
+                    correcto = true;
+                }
+            }
+
+            this.Database = "equipo13_coma";
+            this.server = T_server; // "localhost";
+            this.user = T_user; // "root";
+            this.clave = T_password; // "";
+            this.port = T_port; //"3306";
+        }
 
         public MySqlConnection CrearConexion()
         {
@@ -16,18 +65,19 @@ namespace ClubDeportivoEquipo13.Datos
             try
             {
                 cadena.ConnectionString =
-                    "Server=localhost;" +
-                    "Database=equipo13_coma;" +
-                    "User Id=root;" +
-                    "Password=root;" +
-                    "Port=3306;";
+                    "Server=" + this.server + ";" +
+                    "Database=" + this.Database + ";" +
+                    "User Id=" + this.user + ";" +
+                    "Password=" + this.clave + ";" +
+                    "Port=" + this.port + ";";
 
-                return cadena;
+                //return cadena;
             }
             catch (Exception ex)
             {
                 throw new Exception("Error al crear la conexión: " + ex.Message);
             }
+            return cadena;
         }
 
         public static Conexion getInstancia()
