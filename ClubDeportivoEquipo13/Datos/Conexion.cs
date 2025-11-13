23edc5583193d8cc3a3eef72c5bf5a7ed52717b2
Agu <aguscp91@gmail.com>
2025-11-13 03:18:36 -0300

    La conexion ahora se hace al iniciar el programa, y los datos de conexion son en un unico formulario.

--- ClubDeportivoEquipo13/Datos/Conexion.cs
+++ ClubDeportivoEquipo13/Datos/Conexion.cs
@@ -6,80 +6,36 @@ namespace ClubDeportivoEquipo13.Datos
 {
     public class Conexion
     {
-        private string Database;
-        private string server;
-        private string port;
-        private string user;
-        private string clave;
         private static Conexion instancia = null;
-        
-        private Conexion() {
 
-            bool correcto = false;
-            int mensaje;
+        public static string Database { get; set; } = "equipo13_coma";
+        public static string Server { get; set; } = "";
+        public static string Port { get; set; } = "";
+        public static string User { get; set; } = "";
+        public static string Clave { get; set; } = "";
 
-            string T_server = "Server";
-            string T_user = "User";
-            string T_password = "Password";
-            string T_port = "Port";
-
-            while (correcto != true)
-            {
-                // Armamos los cuadros de dialogo para el ingreso de datos
-                T_server = Microsoft.VisualBasic.Interaction.InputBox
-                ("ingrese servidor", "DATOS DE INSTALACIÓN MySQL");
-                T_user = Microsoft.VisualBasic.Interaction.InputBox
-                ("ingrese usuario", "DATOS DE INSTALACIÓN MySQL");
-                T_password = Microsoft.VisualBasic.Interaction.InputBox
-                ("ingrese clave", "DATOS DE INSTALACIÓN MySQL");
-                T_port = Microsoft.VisualBasic.Interaction.InputBox
-                ("ingrese puerto", "DATOS DE INSTALACIÓN MySQL");
-
-                mensaje = (int)MessageBox.Show("su ingreso: SERVIDOR = " + T_server +
-                    " USUARIO: " + T_user +
-                    " CLAVE: " + T_password +
-                    " PUERTO= " + T_port, 
-                    "AVISO DEL SISTEMA", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (mensaje != 6) // el valor 6 corresponde al SI
-                {
-                    MessageBox.Show("INGRESE NUEVAMENTE LOS DATOS");
-                    correcto = false;
-                }
-                else
-                {
-                    correcto = true;
-                }
-            }
-
-            this.Database = "equipo13_coma";
-            this.server = T_server; // "localhost";
-            this.user = T_user; // "root";
-            this.clave = T_password; // "";
-            this.port = T_port; //"3306";
-        }
-
+        private Conexion() { }
         public MySqlConnection CrearConexion()
         {
-            MySqlConnection cadena = new MySqlConnection();
-
             try
             {
-                cadena.ConnectionString =
-                    "Server=" + this.server + ";" +
-                    "Database=" + this.Database + ";" +
-                    "User Id=" + this.user + ";" +
-                    "Password=" + this.clave + ";" +
-                    "Port=" + this.port + ";";
-
-                //return cadena;
+                string ConnectionString =
+                    $"Server={Server};Database={Database};User Id= {User};Password={Clave};Port={Port};";
+                return new MySqlConnection(ConnectionString);
             }
             catch (Exception ex)
             {
                 throw new Exception("Error al crear la conexión: " + ex.Message);
             }
-            return cadena;
         }
 
+        public void Configurar(string server, string user , string clave, string port)
+        {
+            Server = server;
+            User = user;
+            Clave = clave;
+            Port = port;
+        }
         public static Conexion getInstancia()
         {
             if (instancia == null)
---
+++ ClubDeportivoEquipo13/Forms/frmConexionDatos.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Configuration;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ClubDeportivoEquipo13.Datos
+{
+    partial class frmConexionDatos : Form
+    {
+        public string Servidor { get; private set; } = "";
+        public string Usuario { get; private set; } = "";
+        public string Clave { get; private set; } = "";
+        public string Puerto { get; private set; } = "";
+
+        public frmConexionDatos()
+        {
+            InitializeComponent();
+            ConfigurarEventos();
+        }
+
+        private void ConfigurarEventos()
+        {
+            txtServidor.Text = "localhost";
+            txtServidor.ForeColor = Color.Gray;
+            txtServidor.Enter += (sender, e) =>
+            {
+                if (txtServidor.Text == "localhost")
+                {
+                    txtServidor.Text = "";
+                    txtServidor.ForeColor = Color.Black;
+                }
+            };
+
+            txtServidor.Leave += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(txtServidor.Text))
+                {
+                    txtServidor.Text = "localhost";
+                    txtServidor.ForeColor = Color.Gray;
+                }
+            };
+
+            txtUsuario.Text = "root";
+            txtUsuario.ForeColor = Color.Gray;
+
+            txtUsuario.Enter += (sender, e) =>
+            {
+                if (txtUsuario.Text == "root")
+                {
+                    txtUsuario.Text = "";
+                    txtUsuario.ForeColor = Color.Black;
+                }
+            };
+
+            txtUsuario.Leave += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(txtUsuario.Text))
+                {
+                    txtUsuario.Text = "root";
+                    txtUsuario.ForeColor = Color.Gray;
+                }
+            };
+
+            txtPuerto.Text = "3306";
+            txtPuerto.ForeColor = Color.Gray;
+
+            txtPuerto.Enter += (sender, e) =>
+            {
+                if (txtPuerto.Text == "3306")
+                {
+                    txtPuerto.Text = "";
+                    txtPuerto.ForeColor = Color.Black;
+                }
+            };
+
+            txtPuerto.Leave += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(txtPuerto.Text))
+                {
+                    txtPuerto.Text = "3306";
+                    txtPuerto.ForeColor = Color.Gray;
+                }
+            };
+
+            btnAceptar.Click += btnAceptar_Click;
+        }
+
+        private void btnAceptar_Click(Object sender, EventArgs e)
+        {
+            Servidor = txtServidor.Text;
+            Usuario = txtUsuario.Text;
+            Clave = txtClave.Text;
+            Puerto = txtPuerto.Text;
+
+            String cadenaConexion = $"Server={Servidor};Database=equipo13_coma;User Id={Usuario};Password={Clave};Port={Puerto};";
+
+            try
+            {
+                using (MySql.Data.MySqlClient.MySqlConnection prueba = new MySql.Data.MySqlClient.MySqlConnection(cadenaConexion))
+                {
+                    prueba.Open();
+                }
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                this.DialogResult = DialogResult.Cancel;
+                this.Close();
+            }
+        }
+    }
+}
